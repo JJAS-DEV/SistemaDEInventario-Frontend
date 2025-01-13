d7eb896bdeb35bc7f7f8840e7b7c08ad894b77eb
jjas <ayosuajon4@gmail.com>
2025-01-13 18:27:55 -0300

    todoFuncionaAqui

--- src/app/components/user-app.component.ts
+++ src/app/components/user-app.component.ts
@@ -12,6 +12,8 @@ import { error } from 'console';
 import { state } from '@angular/animations';
 import { AuthService } from '../services/auth.service';
 import { response } from 'express';
+import { Store } from '@ngrx/store';
+import { add, find, findAll, remove, setPaginator, update } from '../store/users.actions';
 
 @Component({
   selector: 'user-app',
@@ -22,29 +24,29 @@ import { response } from 'express';
 })
 export class UserAppComponent implements OnInit {
   users: User[] = [];
-
   paginator: any={};
-  constructor(private service: UserService, private router: Router,
+  user!:User; 
+
+  constructor(private service: UserService,
+    private store:Store<{users:any}>
+    ,private router: Router,
   private sharingData: SharingDataService,
   private authService:AuthService, 
-  private route:ActivatedRoute
+  private route:ActivatedRoute) {
 
+    this.store.select('users').subscribe(state=>{
+      this.users=state.users;
+      this.paginator=state.paginator;
+      this.user={... state.user};
 
-) {
+    })
+
      
 
   }
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params=>{
-      const page=+(params.get('page')|| '0');
+   
 
-      // 0this.service.findAllPageable(page).subscribe(pageable => this.users = pageable.content as User[]);
-
-
-
-    })
-
-    // this.service.findAll().subscribe(users => this.users = users);
 
     this.addUser();
     this.removeUser();
@@ -104,8 +106,10 @@ export class UserAppComponent implements OnInit {
   }
   pageUsersEvent(){
     this.sharingData.pageUsersEventEmitter.subscribe(pageable=>{
-      this.users=pageable.users;
-      this.paginator= pageable.paginator;
+      // this.users=pageable.users;
+      // this.paginator= pageable.paginator;
+      this.store.dispatch(findAll({users:pageable.users}))
+      this.store.dispatch(setPaginator({paginator:pageable.paginator}))
     })
   }
 
@@ -113,8 +117,9 @@ export class UserAppComponent implements OnInit {
 
     this.sharingData.findUserByIdEventEmitter.subscribe(id=>
     {
-      const user =this.users.find(user=> user.id== id);
-      this.sharingData.selectUserEventEmitter.emit(user);
+      // const user =this.user8s.find(user=> user.id== id);
+      this.store.dispatch(find({id})) 
+      this.sharingData.selectUserEventEmitter.emit(this.user);
     }
     )
 
@@ -127,11 +132,12 @@ export class UserAppComponent implements OnInit {
 
       if (user.id > 0) {
         this.service.update(user).subscribe({
-          next: (userUpdated) => {
+          next: (userUpdate) => {
             // Actualizar la lista de usuarios
-            this.users = this.users.map(u => (u.id === userUpdated.id) ? { ...userUpdated } : u);
-      
+            // this.users = this.users.map(u => (u.id === userUpdated.id) ? { ...userUpdated } : u);
+            
             // Redirigir al listado de usuarios
+            this.store.dispatch(update({userUpdate})) 
             this.router.navigate(['/users/page/'+this.paginator.number]),{
             state:{  users: this.users,
               paginator: this.paginator
@@ -139,7 +145,7 @@ export class UserAppComponent implements OnInit {
             
       Swal.fire({
         title: "actualizado",
-        text: "Usuario"+userUpdated.name+" editado con exito ",
+        text: "Usuario"+userUpdate.name+" editado con exito ",
         icon: "success"
       });
           },
@@ -155,13 +161,14 @@ export class UserAppComponent implements OnInit {
       }
          else {
         this.service.create(user).subscribe({
-          
+           
           next:(userNew) =>{
 
           
           console.log(userNew)
 
-          this.users = [... this.users, { ...userNew}];
+          // this.users = [... this.users, { ...userNew}];
+          this.store.dispatch(add({userNew}));
           Swal.fire({
             title: "guardado",
             text: "Usuario"+userNew.name+" guardado con exito ",
@@ -207,7 +214,8 @@ export class UserAppComponent implements OnInit {
       }).then((result) => {
         if (result.isConfirmed) {
           this.service.remove(id).subscribe(()=>{
-            this.users = this.users.filter(user => user.id != id);
+            // this.users = this.users.filter(user => user.id != id);
+            this.store.dispatch(remove({id}));
             this.router.navigate(['/users/create'], { skipLocationChange: true }).then(() => {
               this.router.navigate(['/users']),{
                 state:{  users: this.users,
