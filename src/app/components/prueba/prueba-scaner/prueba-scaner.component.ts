9f1f0b5150b0460e92a7a6ef5fd7bcfffb49083f
jjas <ayosuajon4@gmail.com>
2025-04-20 14:05:21 -0400

    scaneoYgeneracionCompleto

--- src/app/components/prueba/prueba-scaner/prueba-scaner.component.ts
+++ src/app/components/prueba/prueba-scaner/prueba-scaner.component.ts
@@ -1,21 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { QRCodeModule } from 'angularx-qrcode';
+import { NgxScannerQrcodeComponent,LOAD_WASM } from 'ngx-scanner-qrcode';
 
 
 @Component({
   selector: 'app-prueba-scaner',
   standalone: true,
-  imports: [ QRCodeModule],
+  imports: [CommonModule, QRCodeModule,NgxScannerQrcodeComponent,ReactiveFormsModule],
   templateUrl: './prueba-scaner.component.html',
   styleUrl: './prueba-scaner.component.css'
 })
-export class PruebaScanerComponent {
+
+export class PruebaScanerComponent implements  AfterViewInit  {
   public myAngularxQrCode: string = '';
-  public qrCodeDownloadLink: SafeUrl = '';  // Mantener SafeUrl para seguridad
+  public qrCodeDownloadLink: SafeUrl = ''; 
+  resultado!:any;
+
+  @ViewChild('action', { static: false }) action!: NgxScannerQrcodeComponent;
+  scannedData: string | null = null;
+
+  // Mantener SafeUrl para seguridad
 
   constructor(private sanitizer: DomSanitizer) {
-    this.myAngularxQrCode = 'Your QR code data string';  // Datos para el QR
+    this.myAngularxQrCode = 'jjjas'; 
+        LOAD_WASM('assets/wasm/ngx-scanner-qrcode.wasm').subscribe();
+
+    // Datos para el QR
+  }
+  ngAfterViewInit(): void {
+    this.action.data.subscribe((data: any) => {
+      if (data?.length > 0) {
+        // data[0].value es el valor escaneado
+        this.scannedData = data[0].value;
+        console.log('Código escaneado:', this.scannedData);
+      }
+    });
   }
 
   // Recibe el SafeUrl generado por el componente QR
@@ -24,7 +46,7 @@ export class PruebaScanerComponent {
   }
 
 
-  
+
 }
 
 // File: app.component.html
